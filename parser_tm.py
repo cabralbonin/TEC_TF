b7e1a464dbd5f7909e10bcdd2325d6d07256cf19
Claudinei Cabral <113262182+cabralbonin@users.noreply.github.com>
2024-06-01 13:35:30 -0300

    Update parser_tm.py

--- parser_tm.py
+++ parser_tm.py
@@ -5,7 +5,7 @@
     Para fazer a conversão, o arquivo de entrada deve estar
     em formato .in e no mesmo diretório do arquivo parser_tm.py. 
     
-    O arquivo de entrada não deve conter espaços e comentários.
+    O arquivo de entrada não deve conter espaços, linhas em branco e comentários.
     
     Para selecionar o arquivo a ser convertido basta inserir o nome
     do arquivo na função convert_tm().
